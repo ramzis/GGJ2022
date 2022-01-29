fdebfb1c4879bd3aaa3248019deb21b210d455e2
112kutiko <tadask04@gmail.com>
2022-01-29 21:36:48 +0200

    a

--- Assets/Scripts/in_game_control.cs
+++ Assets/Scripts/in_game_control.cs
@@ -2,39 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-
+using UnityEngine.SceneManagement;
 public class in_game_control : MonoBehaviour
 {
     [SerializeField]
     List<GameObject> ui_setting;
+    [SerializeField]
     NetworkManager networkManager;
+    private Scene scene;
     // Start is called before the first frame update
     void Start()
     {
         //List <GameObject> temp1 = new List<GameObject>(GameObject.Find("NetworkManager"));
         networkManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
 
-        Debug.Log(networkManager.mode);
-        if(networkManager.mode.ToString() == "Host") {
-            ui_setting[1].SetActive(true);
-            Debug.Log(networkManager.mode);
-        }
-        else if (networkManager.mode.ToString() == "ClientOnly")
-        {
-            ui_setting[0].SetActive(true);
-            Debug.Log(networkManager.mode);
-        }
-        else
-        {
-            Debug.Log(networkManager.mode);
-        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        Debug.Log(networkManager.mode);
+        scene = SceneManager.GetActiveScene();
+        if (scene.name == "Main")
+        {
+            if (ui_setting[1].activeSelf == false && ui_setting[0].activeSelf == false)
+            {
+                Debug.Log(networkManager.mode);
+                if (networkManager.mode.ToString() == "Host")
+                {
+                    ui_setting[1].SetActive(true);
+                    Debug.Log(networkManager.mode);
+                }
+                else if (networkManager.mode.ToString() == "ClientOnly")
+                {
+                    ui_setting[0].SetActive(true);
+                    Debug.Log(networkManager.mode);
+                }
+                else
+                {
+                    Debug.Log(networkManager.mode);
+                }
+            }
+        }
     }
+//    public void OnServerConnect(NetworkConnection a){
+   ///     Debug.Log(a.connectionId);
+///}
     public void leftHost()
     {
         networkManager.StopClient();
@@ -43,8 +57,5 @@ public class in_game_control : MonoBehaviour
     {
         networkManager.StopHost();
     }
-    public void EcoleftClient()
-    {
-        //networkManager.StopHost();
-    }
+     
 }
