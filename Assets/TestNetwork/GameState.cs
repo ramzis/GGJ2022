c849fdff5225233010cc70ca6f3c7ba2b6300f37
Eimantas <eima.eima1244@gmail.com>
2022-01-29 15:33:29 +0200

    Networking

--- Assets/TestNetwork/GameState.cs
+++ Assets/TestNetwork/GameState.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GameState : MonoBehaviour
+using Mirror;
+public class GameState : NetworkBehaviour
 {
     public Player P1, P2;
 
-    
+    private void Start()
+    {
+        InvokeRepeating("Update2", 0.5f, 0.5f);
+    }
+    private void Update2()
+    {
+        if(GameObject.FindGameObjectsWithTag("Player").Length == 2)
+        {
+            CancelInvoke("Update2");
+            RpcStartGame();
+        }   
+    }
 
-    public void StartGame()
+    public void RpcStartGame()
     {
         Debug.Log("As cia3");
-        InvokeRepeating("Beat", 1, 1);
+        if(GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<NetworkIdentity>().netId == 1)
+        {
+            P1 = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<Player>();
+            P2 = GameObject.FindGameObjectsWithTag("Player")[1].GetComponent<Player>();
+        }
+        else
+        {
+            P2 = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<Player>();
+            P1 = GameObject.FindGameObjectsWithTag("Player")[1].GetComponent<Player>();
+        }
+        InvokeRepeating("Beat", 0.5f, 0.5f);
+        
     }
+
+
+
 
     void Beat()
     {
--- Assets/TestNetwork/NetworkManagerGame.cs
+++ Assets/TestNetwork/NetworkManagerGame.cs
@@ -13,21 +13,27 @@ public class NetworkManagerGame : NetworkManager
         GameObject player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
         NetworkServer.AddPlayerForConnection(conn, player);
 
-        if (numPlayers == 1)
-        {
-            Debug.Log("As cia");
-            gamestate = GameObject.FindGameObjectWithTag("GameState").GetComponent<GameState>();
-            gamestate.P1 = player.GetComponent<Player>();
+        //GameObject gamestate2 = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "GameState"));
+        //NetworkServer.Spawn(gamestate2);
+        //gamestate = gamestate2.GetComponent<GameState>;
 
-        }
+        //if (numPlayers == 1)
+        //{
+        //    Debug.Log("As cia");
+        //    gamestate = GameObject.FindGameObjectWithTag("GameState").GetComponent<GameState>();
+        //    gamestate.P1 = player.GetComponent<Player>();
+        //    //gamestate.RpcStartGame();
 
-        // spawn ball if two players
-        if (numPlayers == 2)
-        {
-            Debug.Log("As cia2");
-            gamestate.P2 = player.GetComponent<Player>();
-            gamestate.StartGame();
+        //}
 
-        }
+        //// spawn ball if two players
+        //if (numPlayers == 2)
+        //{
+        //    Debug.Log("As cia2");
+        //    gamestate.P2 = player.GetComponent<Player>();
+        //    gamestate.RpcStartGame();
+
+        //}
+        
     }
 }
