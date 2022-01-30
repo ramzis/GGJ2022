b85f5b0de4c9104dfc683a81db38380e0a60bbe3
Tadas Audinis <ramzis@github.com>
2022-01-30 12:41:59 +0200

    Update EndGame.cs

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -1,10 +1,27 @@
+using System;
+using System.Collections;
 using Mirror;
 using UnityEngine;
 
 public class EndGame : MonoBehaviour
 {
+    private bool canClose;
+    
+    private void Start()
+    {
+        canClose = false;
+        StartCoroutine(Delay());
+    }
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(2f);
+        canClose = true;
+    }
+
     private void Update()
     {
+        if (!canClose) return;
         if (Input.anyKeyDown)
         {
             var manager = FindObjectOfType<NetworkManager>();
