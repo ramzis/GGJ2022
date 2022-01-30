7217e57a79e4c7cceb9df8a2a3ca2c0789516ab3
Tadas Audinis <ramzis@github.com>
2022-01-30 06:12:04 +0200

    Update grid

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -9,6 +9,11 @@ public class Grid : MonoBehaviour
     [SerializeField]
     private int size;
 
+    private GameObject[,] tiles;
+
+    [Range(0,1)]
+    public float amplitude;
+    
     private void Start()
     {
         Generate();
@@ -16,11 +21,24 @@ public class Grid : MonoBehaviour
 
     private void Generate()
     {
+        var parentTile = new GameObject();
+        parentTile.name = "Tiles";
+        tiles = new GameObject[8, 8];
         for(int y=0; y<size; y++)
             for(int x=0; x<size; x++)
             {
-                GameObject go = Instantiate(tile);
-                tile.transform.position = new Vector3(x, 0, y);
+                GameObject go = Instantiate(tile, new Vector3(x, 0, y), Quaternion.identity, parentTile.transform);
+                tiles[y,x] = go;
+            }
+    }
+
+    private void FixedUpdate()
+    {
+        for(int y=0; y<size; y++)
+            for (int x = 0; x < size; x++)
+            {
+                tiles[y, x].transform.position =
+                    new Vector3(x, (Mathf.Cos(x * Time.time) + Mathf.Sin(y * Time.time)) * amplitude, y);
             }
     }
 }
